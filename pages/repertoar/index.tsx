3ce1350deed13ebb307407541bf65557681ac11e
Luke the Puke <lukas.sahula@icloud.com>
2021-03-28 09:24:32 +0200

    Add production images to the mix

--- pages/repertoar/index.tsx
+++ pages/repertoar/index.tsx
@@ -1,22 +1,46 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import Layout from '../../components/Layout'
 import { getAllSnippets } from '../../lib/markdown'
 
 interface Preview {
+  slug: string;
   title: string;
   writer: string;
+  images: string[];
 }
 
 const Repertoar = ({ previews }: { previews: Preview[] }) => {
   console.log(previews);
   return (
     <Layout title="Repertoár">
-      {previews.map(p => <div key={p.title}>{p.title}</div>)}
+      {previews.map(p => {
+        const thumbnail = p.images[Math.floor(Math.random() * p.images.length)];
+        return (
+          <Link href={`/repertoar/${p.slug}`} key={p.slug}>
+            <a>
+              <Image
+                src={`/productions/${p.slug}/images/${thumbnail}`}
+                alt={p.title}
+                height={150}
+                width={250}
+              />
+              <div>
+                {p.title}
+              </div>
+              <div>
+                {p.writer}
+              </div>
+            </a>
+          </Link>
+        )})}
     </Layout>
   );
 };
 
 export async function getStaticProps() {
-  const previews = getAllSnippets(['title', 'writer'])
+  const previews = getAllSnippets(['slug', 'title', 'writer', 'images'])
   return {
     props: {
       previews: previews,
