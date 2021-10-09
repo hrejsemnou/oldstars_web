37d673c7e3d4d9791095ebd87fd5659d8c01ad08
Luke the Puke <lukas.sahula@icloud.com>
2021-10-09 16:47:53 +0200

    add members

--- pages/herecke-studio/clenove.jsx
+++ pages/herecke-studio/clenove.jsx
@@ -1,8 +1,45 @@
-import Layout from '../../components/Layout'
+import Image from 'next/image';
 
-const Home = () => (
-  <Layout title="OLDStars > Herecké studio > Členové" />
+import Layout from '../../components/Layout'
+import { getMembers } from '../../lib/markdown';
+
+import styles from './Soubory.module.scss';
+
+const Members = ({ members }) => (
+  <Layout title="OLDStars > Herecké studio > Členové">
+    <h2>Členové</h2>
+    {members.map(member => (
+      <>
+        <h3>{member.name}</h3>
+        <div className={styles.soubor}>
+          <div>
+            <Image
+              layout="fixed"
+              key="tstanek"
+              src={`/members/${member.slug}/images/index.jpg`}
+              alt={`${member.slug}-profil`}
+              height={135}
+              width={240}
+            />
+          </div>
+          <div className={styles.souborInfo}>
+            <span>
+              {member.content}
+            </span>
+          </div>
+        </div>
+      </>
+    ))}
+  </Layout>
 )
 
-export default Home
+export async function getStaticProps() {
+  const members = getMembers(['slug', 'name', 'content'])
+  return {
+    props: {
+      members: members,
+    }
+  }
+}
 
+export default Members
---
+++ public/members/member1/index.md
@@ -0,0 +1,5 @@
+---
+name: Herec Krásný
+---
+
+"Desítky mrtvých. To mě nezajímá. Přepnu jinam.” Tři ženy, tři národnosti, nespočet úhlů pohledu. Jedna herečka. Zbývá jeden rok, deset dnů a osm hodin do výstřelu v baru Rishon-Lezion v Tel Avivu. Monodrama italského autora uvádíme v bytovém divadle v Košické v české premiéře.
