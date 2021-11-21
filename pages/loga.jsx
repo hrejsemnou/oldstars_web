8aa044b52d27e34cc30439eca8ced59ddb32ba5d
Luke the Puke <lukas.sahula@icloud.com>
2021-11-21 15:47:12 +0100

    Press a loga

---
+++ pages/loga.jsx
@@ -0,0 +1,27 @@
+import Layout from '../components/Layout'
+
+const Home = () => (
+  <Layout title="Dokumenty">
+    <h2>Loga ke stažení</h2>
+    <p>
+      <a href="/loga/h2o_logo_baw.jpeg" download>Logo H2O - Černobílé</a>
+    </p>
+    <p>
+      <a href="/loga/h2o_logo_cmyk.jpeg" download>Logo H2O - Barevné</a>
+    </p>
+    <p>
+      <a href="/loga/harold_logo_baw.jpeg" download>Logo Harold - Černobílé</a>
+    </p>
+    <p>
+      <a href="/loga/harold_logo_cmyk.jpeg" download>Logo Harold - Barevné</a>
+    </p>
+    <p>
+      <a href="/loga/oldstars_logo_baw.jpeg" download>Logo OLDstars - Černobílé</a>
+    </p>
+    <p>
+      <a href="/loga/oldstars_logo_cmyk.jpeg" download>Logo OLDstars - Barevné</a>
+    </p>
+  </Layout>
+)
+
+export default Home
--- pages/press.jsx
+++ pages/press.jsx
@@ -1,7 +1,44 @@
+import Link from 'next/link';
+
 import Layout from '../components/Layout'
 
 const Home = () => (
-  <Layout title="OLDStars > Press" />
+  <Layout title="OLDStars > Press">
+    <h2>Informace pro novináře</h2>
+    <p>
+      <a href="mailto:press@oldstars.cz">press@oldstars.cz</a>
+    </p>
+    <p>
+      +420 606 865 237
+    </p>
+    <p>
+      <strong>PR OLDstars: </strong><a href="mailto:nicol@oldstars.cz">nicol@oldstars.cz</a>
+    </p>
+    <p>
+      <strong>PR projektů: </strong>Klára Vaňkátová, <a href="mailto:klara.vankatova.kv@seznam.cz">klara.vankatova.kv@seznam.cz</a>
+    </p>
+    <Link href="/loga">
+      <a>
+        Loga ke stažení
+      </a>
+    </Link>
+    <h2>Tiskové zprávy</h2>
+    <p>
+      <a href="/tiskovky/TZ_-_Festival_studentskeho_divadla_v_Celetne_2019.pdf" download>TZ Festival studentského divadla v Celetné 2019</a>
+    </p>
+    <p>
+      <a href="/tiskovky/TZ_OLDstars_on_the_ROUD_2019.pdf" download>TZ benefičního festivalu OLDstars on the Roud 2019</a>
+    </p>
+    <p>
+      <a href="/tiskovky/TZ-NIKDY-OLDstars_bytové_divadlo_kosicka.pdf" download>TZ inscenace NIKDY se stěhuje do bytového divadla v Košické</a>
+    </p>
+    <p>
+      <a href="/tiskovky/TZ_festival_studentského_divadla_Celetná_2018.pdf" download>TZ Festival studentských divadel v Divadle v Celetne 2018</a>
+    </p>
+    <p>
+      <a href="/tiskovky/TZ_OLDstars_se_vrací_z_Žižkova_do_Vršovic_2018.pdf" download>TZ OLDstars se vrací ze Žižkova do Vršovic 2018</a>
+    </p>
+  </Layout>
 )
 
 export default Home
