f9f782c3b0ea2d2c1eb1e128083b14d8d3d7dec8
klaravank <93821300+klaravank@users.noreply.github.com>
2021-12-27 08:33:21 +0100

    Update partneri.jsx

--- pages/partneri.jsx
+++ pages/partneri.jsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout'
 
 const Home = () => (
   <Layout title="OLDStars > Partneři">
-    <p>Partnery a podporovateli umělecké skupiny OLDstars jsou Nadace Život umělce, Státní fond kultury ČR, Magistrát hl. m. Prahy, Městská část Praha 10, Městská část Praha 1, webový portál Goout.net, divadelní portál scena.cz a město Roudnice nad Labem. Děkujeme za jejich přízeň!</p>
+    <p style="text-align:center">Partnery a podporovateli umělecké skupiny OLDstars jsou Nadace Život umělce, Státní fond kultury ČR, Magistrát hl. m. Prahy, Městská část Praha 10, Městská část Praha 1, webový portál Goout.net, divadelní portál scena.cz a město Roudnice nad Labem. Děkujeme za jejich přízeň!</p>
   </Layout>
 )
 
