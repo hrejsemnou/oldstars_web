0b38b2c3777e58f896514e9d0ef1d14f52c58c38
klaravank <93821300+klaravank@users.noreply.github.com>
2021-12-27 07:43:12 +0100

    Update and rename zajezdy.jsx to zajezdy a turne.jsx

--- pages/projekty/zajezdy a turne.jsx
+++ pages/projekty/zajezdy a turne.jsx
@@ -1,7 +1,7 @@
 import Layout from '../../components/Layout'
 
 const Home = () => (
-  <Layout title="OLDStars > Projekty > Zájezdy">
+  <Layout title="OLDStars > Projekty > Zájezdy a turné">
     <p>Omlouvám se, na této sekci se pracuje.</p>
   </Layout>
 )
