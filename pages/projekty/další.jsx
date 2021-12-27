681bb948bf938e4c1f2764e012e65c1702fde433
klaravank <93821300+klaravank@users.noreply.github.com>
2021-12-27 07:41:46 +0100

    Update and rename ostatni.jsx to další.jsx

--- pages/projekty/další.jsx
+++ pages/projekty/další.jsx
@@ -1,7 +1,7 @@
 import Layout from '../../components/Layout'
 
 const Home = () => (
-  <Layout title="OLDStars > Projekty > Ostatní">
+  <Layout title="OLDStars > Projekty > Další">
     <p>Omlouváme se, na této sekci se pracuje</p>
   </Layout>
 )
