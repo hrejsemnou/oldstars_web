953e80781b4229935d9ee0cea346132276b8e83f
klaravank <93821300+klaravank@users.noreply.github.com>
2022-01-25 19:41:02 +0100

    Update o-nas.jsx

--- pages/oldstars/o-nas.jsx
+++ pages/oldstars/o-nas.jsx
@@ -6,10 +6,22 @@ const Home = () => (
     <h3>Aktuálně</h3>
     <article>
       <p>
-        OLDstars z. s. jsou nezávislá umělecká skupina sdružující přes stovku studentů, převážně ze středních a vysokých uměleckých škol. Za osmnáct let své existence nastudovali přes 300 inscenací a uspořádali 18 ročníků divadelních festivalů. Mezi ně patří benefiční festival OLDstars on the ROUD i Festival studentských divadel v pražském Divadle v Celetné, které se pravidelně uskutečňují dodnes. Připravují divadelní turné a jiné kulturní projekty, úspěšně se účastní soutěžních festivalů po celé republice. V rámci své pražské činnosti provozují dva prostory, sklep HarOLD v Charkovské ulici ve Vršovicích a Bytové divadlo H2/O v nedaleké Košické ulici. Pravidelně se účastní studentských divadelních přehlídek a tradiční Noci divadel.
+        OLDstars z. s. jsou nezávislá umělecká skupina sdružující přes stovku studentů, převážně ze středních a vysokých uměleckých škol. Za osmnáct let své existence nastudovali přes 300 inscenací a uspořádali 18 ročníků divadelních festivalů. Mezi ně patří benefiční festival OLDstars on the ROUD i Festival studentských divadel v pražském Divadle v Celetné, které se pravidelně uskutečňují dodnes. Pořádají divadelní turné a jiné kulturní projekty po celé republice. V rámci své pražské činnosti provozují ve Vršovicích dva prostory: sklepní HarOLD v Charkovské ulici a Bytové divadlo H2/O v nedaleké Košické ulici. Pravidelně se účastní studentských divadelních přehlídek a tradiční Noci divadel.
       </p>
       <p>
-         Repertoár je široce dramaturgicky rozkročený s důrazem na přípravu budoucích profesionálních umělců - kromě klasických titulů a původních dramatizací si Herecké studio vyzkoušelo i autorskou tvorbu. V posledních sezónách se diváci často potkávají s moderní dramatikou, nejčastěji s tzv. coolness a “in yer face” dramatikou. 
+        Repertoár je široce dramaturgicky rozkročený s důrazem na přípravu budoucích profesionálních umělců - kromě klasických titulů a původních dramatizací si Herecké studio vyzkoušelo i autorskou tvorbu. V posledních sezónách se často potkávají s moderní dramatikou. Idea OLDstars vychází ze specifik studentského - tedy generačního divadla. Současné hry vybízejí k experimentu a rezonují s tématy tvůrců i celé společnosti. 
+      </p>
+      <p>
+        Nezávislé studentské divadlo má právo na názor, pokus i omyl. Chceme být otevřenou platformou studentského kulturního života nejen v Praze.
+      </p>
+      <p>
+        Významným pilířem v aktivitě OLDstars je komplexní příprava uchazečů pro studium a spolupráce se studenty a čerstvými absolventy prestižních vysokých uměleckých škol (DAMU, JAMU, VŠUP, AVU, Arts Management VŠE) v nejrůznějších – kreativních i manažerských oborech. Pro  nejmladší zájemce vzniklo Dětské D studio OLDstars.
+      </p>
+      <p>
+        Inspirativní jsou divadelní potkávání našich studentů s profesionály – v inscenacích OLDstars po boku studentů pohostinsky vystoupili: Ljuba Krbová, Saša Minajev, Kateřina Březinová, Miroslav Hanuš, Daniel Krejčík nebo Elizaveta Maximová.
+      </p>
+      <p>
+        Doménou OLDstars je participace studentů na provozu umělecké skupiny, včetně řízení jednotlivých projektů.  
       </p>
       <p>
         Kromě Hereckého studia skupina nabízí pro zájemce úspěšné přípravné kurzy na umělecké střední a vysoké školy. Pro ty nejmladší vzniklo dětské D studio OLDstars.
