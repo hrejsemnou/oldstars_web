2d7be0d2d876f740e8376cdbae4328ee5dd3b3d3
klaravank <93821300+klaravank@users.noreply.github.com>
2022-01-25 20:07:47 +0100

    Update zajezdy-a-turne.jsx

--- pages/projekty/zajezdy-a-turne.jsx
+++ pages/projekty/zajezdy-a-turne.jsx
@@ -2,7 +2,8 @@ import Layout from '../../components/Layout'
 
 const Home = () => (
   <Layout title="OLDstars > Projekty > Zájezdy a turné">
-    <p>Omlouvám se, na této sekci se pracuje.</p>
+    <p>S našimi inscenacemi rádi vyrazíme i na Vaši akci a do Vašeho města. Nejčastěji cestujeme s monodramaty VŠECHNY BÁJEČNÉ VĚCI nebo MARGARITA. Oblíbená je na zájezdech inscenace TO VŠECHNO ONA nebo STOJÍM V ŘADĚ.</p>
+    <p>Napište si nezávazně o možnosti: zajezdy@oldstars.cz</p>  
   </Layout>
 )
 
