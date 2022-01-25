e11f3a63b0b422aa577b31df7bcbd3582c44f02d
klaravank <93821300+klaravank@users.noreply.github.com>
2022-01-25 20:20:20 +0100

    Update podporte-nas.jsx

--- pages/podporte-nas.jsx
+++ pages/podporte-nas.jsx
@@ -2,7 +2,18 @@ import Layout from '../components/Layout'
 
 const Home = () => (
   <Layout title="OLDstars > Podpořte nás">
-    <p>Omlouvám se, na této sekci se pracuje.</p>
+    <h3>Koho hledáme?</h3>
+    <p>Strategického partnera, který podpoří studentský neziskový kulturní projekt.</p>
+    <p>Mecenáše, který motivuje mladé lidi k aktivnímu trávení času.</p>
+    <p>Parťáka, který rád přiloží ruku k dílu.</p>
+    <h3>Jaká podpora přijde vhod?</h3>
+    <p>Každá! Oceníme „volnou kapacitu“ zaměstnanců, zápůjčku vybavení, přebytkový materiál, příspěvek na provoz. Pomůže nám „levnější tisk“, nebo stará nepoužívaná židle z chalupy.</p>
+    <h3>Co nabízíme?</h3>
+    <p>Podíl na úspěchu originálního projektu studentské organizace.</p>
+    <p>Spolupráci s dynamickým a kreativním týmem mladých profesionálů.</p>
+    <p>Prostor pro zviditelnění skrze naše komunikační kanály (sociální sítě - Facebook, Instagram, propagační materiály - tiskoviny). Možnost prezentace výrobku nebo služby v našich prostorech. Zásah do cílové skupiny studentů v jejím přirozeném prostředí.</p> 
+    <p>Spolupráci s neziskovou organizací, která funguje v kulturním prostředí již 18 let.</p>
+
   </Layout>
 )
 
