3f584baf01936a5963906ed76e531bf898fe158f
klaravank <93821300+klaravank@users.noreply.github.com>
2022-01-26 15:50:09 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -7,24 +7,6 @@ const Home = () => (
     <p>
       Studentská umělecká skupina OLDstars se v létě opět vydá na cesty. Svůj letošní repertoár podělí v červenci mezi několik lokací ve Středočeském kraji.
     </p>
-    <h3>Proběhlé ročníky</h3>
-    <p>Omlouváme se, na této sekci se pracuje</p>
-    <h3>Kontakty</h3>
-    <div>
-      koordinátorka festivalu
-    </div>
-    <div>
-      Elisa Speváková, +420 736 621 558, <a href="mailto:elisa.spevakova@oldstars.cz">elisa.spevakova@oldstars.cz</a>
-    </div>
-    <br />
-    <div>
-      PR a propagace
-    </div>
-    <div>
-      Klára Vaňkátová, +420 733 665 680, <a href="mailto:klara.vankatova@oldstars.cz">klara.vankatova@oldstars.cz</a>
-    </div>
-    <h3>Články</h3>
-    <p>Omlouváme se, na této sekci se pracuje</p>
   </Layout>
 )
 
