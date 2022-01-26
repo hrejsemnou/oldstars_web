02edf0a949ec88b420dce3f9b5c9747856f34b5a
klaravank <93821300+klaravank@users.noreply.github.com>
2022-01-26 15:53:15 +0100

    Update festival-v-roudnici-nad-labem.jsx

--- pages/projekty/festival-v-roudnici-nad-labem.jsx
+++ pages/projekty/festival-v-roudnici-nad-labem.jsx
@@ -2,7 +2,23 @@ import Layout from '../../components/Layout'
 
 const Home = () => (
   <Layout title="OLDstars > Projekty > Festival v Roudnici nad Labem">
-    <p>Omlouváme se, na této sekci se pracuje</p>
+  <h3>Festival v Roudnici nad Labem</h3>  
+    <p>Bývalý augustiniánský klášter v Roudnici nad Labem je dějištěm 8 ročníků benefičního festivalu. Víkendový maraton studentského divadla každý rok v červenci obsadí rozlehlý areál kláštera i veřejný městský prostor. Výtěžek ze vstupného je tradičně věnován na opravy a rozvoj kláštera - zisk z minulých let byl například využit pro vznik Klášterního klubu nebo Ekocentra Zvonice. Diváci mají možnost zhlédnout na tři desítky inscenací ze stálého repertoáru OLDstars a jejich hostů, a to včetně titulů připravovaných přímo pro jednotlivé ročníky festivalu. Pro diváky je tradičně zajištěno občerstvení, toalety i sprchy, spaní ve stanovém městečku v klášterní zahradě a neomezený přístup k pitné vodě.</p>
+    <p>Festival OLDstars on the ROUD 2021 byl podpořen grantem Ministerstvem kultury – Státního fondu kultury. Osmý ročník proběhl pod záštitou hejtmana Ústeckého kraje Ing. Jana Schillera a rektorky Akademie múzických umění PhDr. Ingeborg Radok Žádné. Partnery festivalu byli Kino Sokol a Cukrárna Dortletka. Děkujeme také SOŠ a SOU a Gymnáziu v Roudnici nad Labem a jejím ředitelům Ing. Richardu Červenému a Mgr. Petru Dobešovi za pomoc a podporu při organizaci festivalu.</p>
+    <h3>Kontakty</h3>
+    <div>
+      koordinátorka festivalu
+    </div>
+    <div>
+      Elisa Speváková, +420 736 621 558, <a href="mailto:elisa.spevakova@oldstars.cz">elisa.spevakova@oldstars.cz</a>
+    </div>
+    <br />
+    <div>
+      PR a propagace
+    </div>
+    <div>
+      Klára Vaňkátová, +420 733 665 680, <a href="mailto:klara.vankatova@oldstars.cz">klara.vankatova@oldstars.cz</a>
+    </div>
   </Layout>
 )
 
