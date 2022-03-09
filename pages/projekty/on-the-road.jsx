d1c1db87d83bb233d9fda89c31c682b145f8bd4d
klaravank <93821300+klaravank@users.noreply.github.com>
2022-03-09 14:25:12 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -3,15 +3,16 @@ import Layout from '../../components/Layout'
 const Home = () => (
   <Layout title="OLDstars > Projekty > OLDstars on the ROAD">
     <h2>OLDstars on the ROAD 2022</h2>
-    <h3>8.- 10.7. 2022 bývalý pivovar Sedlec, Kutná Hora</h3>
-    <h3>11.-15. 7. turné po hradech, zámcích a okolí</h3>
-    <h3>16.-17.7. klášter Roudnice nad Labem</h3>
+    <h4>8.- 10.7. 2022 bývalý pivovar Sedlec, Kutná Hora</h4>
+    <h4>11.-15. 7. turné po hradech, zámcích a okolí</h4>
+    <h4>16.-17.7. klášter Roudnice nad Labem</h4>
     <p>
       Studentská umělecká skupina OLDstars se v červenci 2022 opět vydá na cesty. Svůj letošní repertoár podělí mezi několik lokací ve Středočeském kraji. Cestovní léto zahájí víkendovým divadelním festivalem v areálu bývalého pivovaru Sedlec v Kutné hoře. Následně inscenace nabídne pod širým nebem na několika hradech, zámcích a parcích v okolí. Letní turné zakončí v bývalém augustiniánském klášteře v Roudnici nad Labem.
 
       Bývalý pivovar Sedlec u katedrály Panny Marie a sv. Jana Křtitele v Kutné Hoře je již několik let regionálním centrem nezávislé kultury. Druhý červencový víkend jej zamýšlíme zahltit maratonem studentského divadla.  Areál nabízí desítku originálních lokací, těšit se můžete nejen na openair, ale i komorní inscenace v interiérech stodol a ve spletitém podzemí. V jedinečném industriálním objektu vzniknou divadelní projekty přímo na míru jednotlivým prostorům, ale můžete se těšit i na reprízy oblíbených inscenací z repertoáru. Dramaturgie festivalu bude velmi různorodá, uvedeme interpretace klasických textů, díla současných dramatiků, ale i autorské inscenace.
     </p>
          <a href="https://www.goforboom.com/cs/events/oldstars-on-the-road-kutna-hora?fbclid=IwAR0RX5qLeQUuzG-G--keDNZDRGwVAGxHXxMJ-UFizc4Eff1zKRS-bDtKDuA">Vstupenky na festival k zakoupení zde</a>
+        
          <a href="http://www.oldstarsontheroad.cz">www.oldstarsontheroad.cz</a>
   </Layout>
 )
