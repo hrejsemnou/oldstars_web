f9cd10cfc4deabd0d064c1203f63ace222817a8d
klaravank <93821300+klaravank@users.noreply.github.com>
2022-03-14 18:27:45 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -8,7 +8,8 @@ const Home = () => (
     <h4>16.-17.7. klášter Roudnice nad Labem</h4>
     <p>
       Studentská umělecká skupina OLDstars se v červenci 2022 opět vydá na cesty. Svůj letošní repertoár podělí mezi několik lokací ve Středočeském kraji. Cestovní léto zahájí víkendovým divadelním festivalem v areálu bývalého pivovaru Sedlec v Kutné hoře. Následně inscenace nabídne pod širým nebem na několika hradech, zámcích a parcích v okolí. Letní turné zakončí v bývalém augustiniánském klášteře v Roudnici nad Labem.
-
+    </p>
+    <p>
       Bývalý pivovar Sedlec u katedrály Panny Marie a sv. Jana Křtitele v Kutné Hoře je již několik let regionálním centrem nezávislé kultury. Druhý červencový víkend jej zamýšlíme zahltit maratonem studentského divadla.  Areál nabízí desítku originálních lokací, těšit se můžete nejen na openair, ale i komorní inscenace v interiérech stodol a ve spletitém podzemí. V jedinečném industriálním objektu vzniknou divadelní projekty přímo na míru jednotlivým prostorům, ale můžete se těšit i na reprízy oblíbených inscenací z repertoáru. Dramaturgie festivalu bude velmi různorodá, uvedeme interpretace klasických textů, díla současných dramatiků, ale i autorské inscenace.
     </p>
     <p>
@@ -17,6 +18,19 @@ const Home = () => (
     <p>Projekt byl podpořen grantem Státního fondu kultury a grantem Středočeského kraje - děkujeme!</p> 
     <p>
          <a href="http://www.oldstarsontheroad.cz">www.oldstarsontheroad.cz</a>
+    </p>
+    <h3>Jak se dělá divadelní festival?</h3>
+    <p> 
+      *Doprovodný program OLDstars v Kutné Hoře*
+    </p>
+    <p>
+      Program festivalu bude bohatý, inscenovány budou nejen hry klasické, ale i nejnovější kousky od současných dramatiků, nabídnou i pestrou škálu doprovodných programů. Organizovány budou workshopy herectví, cirkusové akrobacie a příměstský tábor "Jak se dělá divadelní festival?".
+    </p>
+    <p>
+      Letní divadelní festival si pochopitelně žádá celou řadu příprav. Účastníci příměstského tábora tak budou mít jedinečnou možnost začlenit se do tvorby festivalu od samého začátku. Každý den přípravného týdne (4.-8. 7. 2022) je pro ně připraven jeden ateliér, který jim umožní nahlédnout na přípravy festivalu optikou nejrůznějších divadelních profesí. Těšit se tak mohou například na seznámení s výkonnou produkcí, propagací a PR, tvůrčím psaním nebo ovládáním divadelní techniky. Dále se zúčastní vybraných divadelních zkoušek a samozřejmě si uměleckou práci vyzkouší na vlastní kůži. Předchozí zkušenost s divadlem není nutná. 
+    </p>
+    <p>
+      Podrobnější informace a přihlášky na e-mailové adrese festival@oldstars.cz. Součástí workshopů i příměstského tábora je festivalová permanentka. Tábor je primárně určen pro účastníky ve věku 13-17 let.
     </p>
   </Layout>
 )
