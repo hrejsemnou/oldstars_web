90fe3e32875c8229bbef95b756c82ca1b1f59e10
klaravank <93821300+klaravank@users.noreply.github.com>
2022-03-14 17:32:04 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -13,10 +13,9 @@ const Home = () => (
     </p>
     <p>
          <a href="https://www.goforboom.com/cs/events/oldstars-on-the-road-kutna-hora?fbclid=IwAR0RX5qLeQUuzG-G--keDNZDRGwVAGxHXxMJ-UFizc4Eff1zKRS-bDtKDuA">Vstupenky na festival k zakoupení zde</a>
-    </p><br><br>
-    <p>Projekt byl podpořen grantem Státního fondu kultury a grantem Středočeského kraje - děkujeme!<br><br> 
-  
-  
+    </p>
+    <p>Projekt byl podpořen grantem Státního fondu kultury a grantem Středočeského kraje - děkujeme!</p> 
+    <p>
          <a href="http://www.oldstarsontheroad.cz">www.oldstarsontheroad.cz</a>
     </p>
   </Layout>
