6ff855b6744f8bf273143c7f46b6fcf61fa2d6ba
klaravank <93821300+klaravank@users.noreply.github.com>
2022-05-04 17:16:54 +0200

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -15,7 +15,8 @@ const Home = () => (
     <p>
          <a href="https://www.goforboom.com/cs/events/oldstars-on-the-road-kutna-hora?fbclid=IwAR0RX5qLeQUuzG-G--keDNZDRGwVAGxHXxMJ-UFizc4Eff1zKRS-bDtKDuA">Vstupenky na festival k zakoupení zde</a>
     </p>
-    <p>Projekt byl podpořen grantem Státního fondu kultury a grantem Středočeského kraje - děkujeme!</p> 
+    <p> Projekt OLDstars on the ROAD 2022 byl podpořen grantem Státního fondu kultury, grantem měst Kutná Hora a Roudnice nad Labem a grantem Středočeského kraje - děkujeme!</p> 
+  
     <p>Letošní ročník proběhne pod záštitou rektora Janáčkovy akademie múzických umění v Brně MgA. Petra Michálka, radního pro oblast kultury, památkové péče a cestovního ruchu Středočeského kraje Mgr. Václava Švendy, radního pro kulturu v Kutné Hoře Mgr. Štěpána Drtiny a vedoucího Katedry pedagogiky na Filozofické fakultě Univerzity Karlovy v Praze Doc. PhDr. Martina Strouhala, Ph.D. Děkujeme! </p>
     <p>
          <a href="http://www.oldstarsontheroad.cz">www.oldstarsontheroad.cz</a>
@@ -33,6 +34,10 @@ const Home = () => (
     <p>
       Podrobnější informace a přihlášky na e-mailové adrese festival@oldstars.cz. Součástí workshopů i příměstského tábora je festivalová permanentka. Tábor je primárně určen pro účastníky ve věku 13-17 let.
     </p>
+    <p>
+      Partnery festivalu OLDstars on the ROAD: Kutná Hora 2022 jsou <a href="https://www.divadlox10.cz/cs/kutna-hora">X10 Kutná Hora</a>, turistická oblast <a href="https://www.kutnohorskokolinsko.cz/">Kutnohorskokolínsko</a>, reklamní agentura <a href="https://lepor.cz/">Lepor</a> a oldservis.cz.
+      Mediálními partnery festivalu jsou servery <a href="https://www.mistnikultura.cz/">Místní kultura</a>, [i-divadlo.cz](https://www.i-divadlo.cz/), <a href="http://www.scena.cz/">Scena.cz</a> a <a href="https://www.kudyznudy.cz/">Kudyznudy.cz</a>.
+    </p>  
   </Layout>
 )
 
