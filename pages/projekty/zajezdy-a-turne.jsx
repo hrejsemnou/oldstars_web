ed39a762fac5127488aac9eeddbd37750d29296c
klaravank <93821300+klaravank@users.noreply.github.com>
2022-06-29 07:10:42 +0200

    Update zajezdy-a-turne.jsx

--- pages/projekty/zajezdy-a-turne.jsx
+++ pages/projekty/zajezdy-a-turne.jsx
@@ -2,8 +2,8 @@ import Layout from '../../components/Layout'
 
 const Home = () => (
   <Layout title="OLDstars > Projekty > Zájezdy a turné">
-    <p><b>Více informací o letošním turné s inscenací MACBETH pořádaným v termínu 11.7. - 17.7. 2022 naleznete na webových stránkách: 
-  <a href="https://www.oldstarsontheroad.cz/turne/">11.-15. 7. turné po hradech, zámcích a okolí</a></b></p>
+    <p><b>Více informací o letošním turné s inscenací MACBETH pořádaným v termínu 11.7. - 17.7. 2022 naleznete na webových stránkách:</b></p>
+    <p> <a href="https://www.oldstarsontheroad.cz/turne/">11.-15. 7. turné po hradech, zámcích a okolí</a></p>
     <p>
         S našimi inscenacemi rádi vyrazíme i na Vaši akci a do Vašeho města. Nejčastěji cestujeme s monodramaty VŠECHNY BÁJEČNÉ VĚCI nebo MARGARITA. Oblíbená je na zájezdech inscenace TO VŠECHNO ONA nebo STOJÍM V ŘADĚ.</p>
     <p>Napište si nezávazně o možnosti: zajezdy@oldstars.cz</p>  
