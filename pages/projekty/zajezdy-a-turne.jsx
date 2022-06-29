aeeb782d98f4b274f887f92711c70f67c85153cb
klaravank <93821300+klaravank@users.noreply.github.com>
2022-06-29 06:54:38 +0200

    Update zajezdy-a-turne.jsx

--- pages/projekty/zajezdy-a-turne.jsx
+++ pages/projekty/zajezdy-a-turne.jsx
@@ -2,7 +2,9 @@ import Layout from '../../components/Layout'
 
 const Home = () => (
   <Layout title="OLDstars > Projekty > Zájezdy a turné">
-    <p>S našimi inscenacemi rádi vyrazíme i na Vaši akci a do Vašeho města. Nejčastěji cestujeme s monodramaty VŠECHNY BÁJEČNÉ VĚCI nebo MARGARITA. Oblíbená je na zájezdech inscenace TO VŠECHNO ONA nebo STOJÍM V ŘADĚ.</p>
+    <p> Více informací o letošním turné pořádaným v termínu 11.7. - 17.7. 2022 naleznete na webových stránkách: <a href="https://www.oldstarsontheroad.cz/turne/">11.-15. 7. turné po hradech, zámcích a okolí</a></p>
+    <p>
+        S našimi inscenacemi rádi vyrazíme i na Vaši akci a do Vašeho města. Nejčastěji cestujeme s monodramaty VŠECHNY BÁJEČNÉ VĚCI nebo MARGARITA. Oblíbená je na zájezdech inscenace TO VŠECHNO ONA nebo STOJÍM V ŘADĚ.</p>
     <p>Napište si nezávazně o možnosti: zajezdy@oldstars.cz</p>  
   </Layout>
 )
