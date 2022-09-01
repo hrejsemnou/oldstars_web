e0543f684027f7c4943e12682afbe7636efef064
klaravank <93821300+klaravank@users.noreply.github.com>
2022-09-01 23:15:43 +0200

    Update festival-studentskych-divadel.jsx

--- pages/projekty/festival-studentskych-divadel.jsx
+++ pages/projekty/festival-studentskych-divadel.jsx
@@ -12,10 +12,40 @@ const Home = () => (
     <h3>Letošní ročník 2022</h3>
     <p>
       Letošní ročník pořádáme tradičně poslední říjnový víkend v Divadle v Celetné. Festivalová přehlídka studentského divadla proběhne ve dnech 27.-29.10.2022.
+    <div>  ČTVRTEK 27. 10. </div>
+
+    18:00 <a href="https://www.oldstarsontheroad.cz/dopisy/">DOPISY K PATĚ STROMU</a>
+
+    20:00 <a href="https://www.oldstars.cz/repertoar/Habbat%20alep">HABBAT ALEP</a>
+
+    22:00 <a href="https://www.oldstars.cz/repertoar/balkon">BALKON</a>
+
+    <div> PÁTEK 28. 10. – státní svátek </div>
+
+    15:00 <a href="https://www.oldstars.cz/repertoar/Dokonal%C3%BD%20partner">DOKONALÝ PARTNER  - Vanabí, Litoměřice</a>
+
+    17:00 <a href="https://www.oldstars.cz/repertoar/vladimir">VLADIMÍR</a>
+
+    19:00 JEN PRO TVÉ DOBRO - DDM  Prahy 3
+
+    21:00 <a href="https://www.oldstars.cz/repertoar/Macbeth22">MACBETH</a>
+
+    23:00 <a href="https://www.oldstarsontheroad.cz/za-zrcadlem/">ZA ZRCADLEM</a>
+
+    <div> SOBOTA 29. 10. </div>
+
+    15:00 <a href="https://www.oldstars.cz/repertoar/bose-nohy">BOSÉ NOHY</a>
+
+    17:00 <a href="https://www.oldstars.cz/repertoar/opil%C3%AD">OPILÍ</a>
+
+    19:00 <a href="https://www.oldstars.cz/repertoar/plice">PLÍCE</a>
+
+    21:00 <a href="https://www.oldstars.cz/repertoar/100-songu">100 SONGŮ</a>
+
+    22:00 <a href="https://www.oldstarsontheroad.cz/mejdan-majdanu/">MEJDAN MAJDANU</a>
+
+    23:00 <a href="https://www.oldstars.cz/repertoar/vztekli-psi">VZTEKLÍ PSI - derniéra</a>
     </p>
-    
-    <h3>Proběhlé ročníky</h3>
-    <p>Omlouváme se, na této sekci se pracuje</p>
     <h3>Kontakty</h3>
     <div>
       produkce festivalu
@@ -30,6 +60,9 @@ const Home = () => (
     <div>
       Elisa Speváková, +420 736 621 558, <a href="mailto:elisa.spevakova@oldstars.cz">elisa.spevakova@oldstars.cz</a>
     </div>
+    <h3>Proběhlé ročníky</h3>
+    <p>Omlouváme se, na této sekci se pracuje</p>
+    
     <h3>Články</h3>
     <p>Omlouváme se, na této sekci se pracuje</p>
   </Layout>
