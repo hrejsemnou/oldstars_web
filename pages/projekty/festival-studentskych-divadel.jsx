e2fe81bb1d31fa5d0e77768df455904f5ffcc3df
klaravank <93821300+klaravank@users.noreply.github.com>
2022-09-01 15:27:21 +0200

    Update festival-studentskych-divadel.jsx

--- pages/projekty/festival-studentskych-divadel.jsx
+++ pages/projekty/festival-studentskych-divadel.jsx
@@ -13,46 +13,7 @@ const Home = () => (
     <p>
       Letošní ročník pořádáme tradičně poslední říjnový víkend v Divadle v Celetné. Festivalová přehlídka studentského divadla proběhne ve dnech 27.-29.10.2022.
     </p>
-    <h4>Na co se letos můžete těšit?</h4>
-    <p>
-      ČTVRTEK 27. 10. 
-
-18:00 [DOPISY K PATĚ STROMU](https://www.oldstarsontheroad.cz/dopisy/)
-
-20:00 [HABBAT ALEP](https://www.oldstars.cz/repertoar/Habbat%20alep)
-
-22:00 [BALKON](https://www.oldstars.cz/repertoar/balkon)
-
-
-PÁTEK 28. 10. – státní svátek
-
-
-15:00 [DOKONALÝ PARTNER  - Vanabí, Litoměřice](https://www.oldstars.cz/repertoar/Dokonal%C3%BD%20partner)
-
-17:00 [VLADIMÍR](https://www.oldstars.cz/repertoar/vladimir)
-
-19:00 JEN PRO TVÉ DOBRO - DDM  Prahy 3
-
-21:00 [MACBETH](https://www.oldstars.cz/repertoar/Macbeth22)
-
-23:00 [ZA ZRCADLEM](https://www.oldstarsontheroad.cz/za-zrcadlem/)
-
-
-
-SOBOTA 29.10.
-
-15:00 [BOSÉ NOHY](https://www.oldstars.cz/repertoar/bose-nohy)
-
-17:00 [OPILÍ](https://www.oldstars.cz/repertoar/opil%C3%AD)
-
-19:00 [PLÍCE](https://www.oldstars.cz/repertoar/plice)
-
-21:00 [100 SONGŮ](https://www.oldstars.cz/repertoar/100-songu)
-
-22:00 [MEJDAN MAJDANU](https://www.oldstarsontheroad.cz/mejdan-majdanu/)
-
-      23:00 <a href="https://www.oldstars.cz/repertoar/vztekli-psi"> VZTEKLÍ PSI - derniéra</a>
-    </p>
+    
     <h3>Proběhlé ročníky</h3>
     <p>Omlouváme se, na této sekci se pracuje</p>
     <h3>Kontakty</h3>
