ed64e99e9728b8a27c61769366878544d972f856
klaravank <93821300+klaravank@users.noreply.github.com>
2022-09-01 23:40:30 +0200

    Update festival-studentskych-divadel.jsx

--- pages/projekty/festival-studentskych-divadel.jsx
+++ pages/projekty/festival-studentskych-divadel.jsx
@@ -15,54 +15,48 @@ const Home = () => (
     </p>
     <h4>  ČTVRTEK 27. 10. </h4>
     <p>
-      <div>
         18:00 <a href="https://www.oldstarsontheroad.cz/dopisy/">DOPISY K PATĚ STROMU</a>
-      </div>
-      <div>
+    </p>
+    <p>
         20:00 <a href="https://www.oldstars.cz/repertoar/Habbat%20alep">HABBAT ALEP</a>
-      </div>
-      <div>
-        22:00 <a href="https://www.oldstars.cz/repertoar/balkon">BALKON</a>
-      </div> 
+    </p>
+    <p>
+        22:00 <a href="https://www.oldstars.cz/repertoar/balkon">BALKON</a> 
     </p>
     <h4> PÁTEK 28. 10. – státní svátek <h4>
     <p>   
-      <div>
         15:00 <a href="https://www.oldstars.cz/repertoar/Dokonal%C3%BD%20partner">DOKONALÝ PARTNER  - Vanabí, Litoměřice</a>
-      </div>
-      <div>
+    </p>
+    <p>
         17:00 <a href="https://www.oldstars.cz/repertoar/vladimir">VLADIMÍR</a>
-      </div>
-      <div>
+    </p>
+    <p>
         19:00 JEN PRO TVÉ DOBRO - DDM  Prahy 3
-      </div>
-      <div>
+    </p>
+    <p>
         21:00 <a href="https://www.oldstars.cz/repertoar/Macbeth22">MACBETH</a>
-      </div>
-      <div>
+    </p>
+    <p>
         23:00 <a href="https://www.oldstarsontheroad.cz/za-zrcadlem/">ZA ZRCADLEM</a>
-      </div>
     </p>  
     <h4> SOBOTA 29. 10. </h4>
     <p>
-      <div>
         15:00 <a href="https://www.oldstars.cz/repertoar/bose-nohy">BOSÉ NOHY</a>
-      </div>
-      <div>
+    </p>
+    <p>
         17:00 <a href="https://www.oldstars.cz/repertoar/opil%C3%AD">OPILÍ</a>
-      </div>
-      <div>
+    </pv>
+    <p>
         19:00 <a href="https://www.oldstars.cz/repertoar/plice">PLÍCE</a>
-      </div>
-      <div>
+    </p>
+    <p>
         21:00 <a href="https://www.oldstars.cz/repertoar/100-songu">100 SONGŮ</a>
-      </div>
-      <div>
+    </p>
+    <p>
         22:00 <a href="https://www.oldstarsontheroad.cz/mejdan-majdanu/">MEJDAN MAJDANU</a>
-      </div>
-      <div>
+    </p>
+    <p>
         23:00 <a href="https://www.oldstars.cz/repertoar/vztekli-psi">VZTEKLÍ PSI - derniéra</a>
-      </div>
     </p>
     <h3>Kontakty</h3>
     <p>
