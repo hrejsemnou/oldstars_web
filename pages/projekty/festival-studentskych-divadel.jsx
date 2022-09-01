b31b28409e18cbffb0a2b2d955c07e46d97ab87d
klaravank <93821300+klaravank@users.noreply.github.com>
2022-09-01 23:31:54 +0200

    Update festival-studentskych-divadel.jsx

--- pages/projekty/festival-studentskych-divadel.jsx
+++ pages/projekty/festival-studentskych-divadel.jsx
@@ -12,54 +12,60 @@ const Home = () => (
     <h3>Letošní ročník 2022</h3>
     <p>
       Letošní ročník pořádáme tradičně poslední říjnový víkend v Divadle v Celetné. Festivalová přehlídka studentského divadla proběhne ve dnech 27.-29.10.2022.
+    </p>
     <h4>  ČTVRTEK 27. 10. </h4>
-    <div>
-      18:00 <a href="https://www.oldstarsontheroad.cz/dopisy/">DOPISY K PATĚ STROMU</a>
-    </div>
-    <div>
-      20:00 <a href="https://www.oldstars.cz/repertoar/Habbat%20alep">HABBAT ALEP</a>
-    </div>
-    <div>
-      22:00 <a href="https://www.oldstars.cz/repertoar/balkon">BALKON</a>
-    </div>  
+    <p>
+      <div>
+        18:00 <a href="https://www.oldstarsontheroad.cz/dopisy/">DOPISY K PATĚ STROMU</a>
+      </div>
+      <div>
+        20:00 <a href="https://www.oldstars.cz/repertoar/Habbat%20alep">HABBAT ALEP</a>
+      </div>
+      <div>
+        22:00 <a href="https://www.oldstars.cz/repertoar/balkon">BALKON</a>
+      </div> 
+    </p>
     <h4> PÁTEK 28. 10. – státní svátek <h4>
-    <div>
-      15:00 <a href="https://www.oldstars.cz/repertoar/Dokonal%C3%BD%20partner">DOKONALÝ PARTNER  - Vanabí, Litoměřice</a>
-    </div>
-    <div>
-      17:00 <a href="https://www.oldstars.cz/repertoar/vladimir">VLADIMÍR</a>
-    </div>
-    <div>
-      19:00 JEN PRO TVÉ DOBRO - DDM  Prahy 3
-    </div>
-    <div>
-      21:00 <a href="https://www.oldstars.cz/repertoar/Macbeth22">MACBETH</a>
-    </div>
-    <div>
-    23:00 <a href="https://www.oldstarsontheroad.cz/za-zrcadlem/">ZA ZRCADLEM</a>
-    </div>
-  
+    <p>   
+      <div>
+        15:00 <a href="https://www.oldstars.cz/repertoar/Dokonal%C3%BD%20partner">DOKONALÝ PARTNER  - Vanabí, Litoměřice</a>
+      </div>
+      <div>
+        17:00 <a href="https://www.oldstars.cz/repertoar/vladimir">VLADIMÍR</a>
+      </div>
+      <div>
+        19:00 JEN PRO TVÉ DOBRO - DDM  Prahy 3
+      </div>
+      <div>
+        21:00 <a href="https://www.oldstars.cz/repertoar/Macbeth22">MACBETH</a>
+      </div>
+      <div>
+        23:00 <a href="https://www.oldstarsontheroad.cz/za-zrcadlem/">ZA ZRCADLEM</a>
+      </div>
+    </p>  
     <h4> SOBOTA 29. 10. </h4>
-    <div>
-      15:00 <a href="https://www.oldstars.cz/repertoar/bose-nohy">BOSÉ NOHY</a>
-    </div>
-    <div>
-      17:00 <a href="https://www.oldstars.cz/repertoar/opil%C3%AD">OPILÍ</a>
-    </div>
-    <div>
-      19:00 <a href="https://www.oldstars.cz/repertoar/plice">PLÍCE</a>
-    </div>
-    <div>
-       21:00 <a href="https://www.oldstars.cz/repertoar/100-songu">100 SONGŮ</a>
-    </div>
-    <div>
-      22:00 <a href="https://www.oldstarsontheroad.cz/mejdan-majdanu/">MEJDAN MAJDANU</a>
-    </div>
-    <div>
-      23:00 <a href="https://www.oldstars.cz/repertoar/vztekli-psi">VZTEKLÍ PSI - derniéra</a>
-    </div>
+    <p>
+      <div>
+        15:00 <a href="https://www.oldstars.cz/repertoar/bose-nohy">BOSÉ NOHY</a>
+      </div>
+      <div>
+        17:00 <a href="https://www.oldstars.cz/repertoar/opil%C3%AD">OPILÍ</a>
+      </div>
+      <div>
+        19:00 <a href="https://www.oldstars.cz/repertoar/plice">PLÍCE</a>
+      </div>
+      <div>
+        21:00 <a href="https://www.oldstars.cz/repertoar/100-songu">100 SONGŮ</a>
+      </div>
+      <div>
+        22:00 <a href="https://www.oldstarsontheroad.cz/mejdan-majdanu/">MEJDAN MAJDANU</a>
+      </div>
+      <div>
+        23:00 <a href="https://www.oldstars.cz/repertoar/vztekli-psi">VZTEKLÍ PSI - derniéra</a>
+      </div>
     </p>
     <h3>Kontakty</h3>
+    <p>
     <div>
       produkce festivalu
     </div>
@@ -73,6 +79,7 @@ const Home = () => (
     <div>
       Elisa Speváková, +420 736 621 558, <a href="mailto:elisa.spevakova@oldstars.cz">elisa.spevakova@oldstars.cz</a>
     </div>
+    </p>
     <h3>Proběhlé ročníky</h3>
     <p>Omlouváme se, na této sekci se pracuje</p>
     
