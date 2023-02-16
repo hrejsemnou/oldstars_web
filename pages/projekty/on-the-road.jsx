fcb6c3589c53307be022de7fee548986fa57d534
klaravank <93821300+klaravank@users.noreply.github.com>
2023-02-16 11:05:07 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -2,41 +2,13 @@ import Layout from '../../components/Layout'
 
 const Home = () => (
   <Layout title="OLDstars > Projekty > OLDstars on the ROAD">
-    <h2>OLDstars on the ROAD 2022</h2>
+    <h2>OLDstars on the ROAD 2023</h2>
     <h4><a href="https://www.oldstarsontheroad.cz/festival/">8.- 10.7. 2022 bývalý pivovar Sedlec, Kutná Hora</a></h4>
     <h4><a href="https://www.oldstarsontheroad.cz/turne/">11.-15. 7. turné po hradech, zámcích a okolí</a></h4>
     <h4><a href="https://www.oldstarsontheroad.cz/roudnice/">16.-17.7. klášter Roudnice nad Labem</a></h4>
     <p>
-      Studentská umělecká skupina OLDstars se v červenci 2022 opět vydá na cesty. Svůj letošní repertoár podělí mezi několik lokací ve Středočeském kraji. Cestovní léto zahájí víkendovým divadelním festivalem v areálu bývalého pivovaru Sedlec v Kutné hoře. Následně inscenace nabídne pod širým nebem na několika hradech, zámcích a parcích v okolí. Letní turné zakončí v bývalém augustiniánském klášteře v Roudnici nad Labem.
-    </p>
-    <p>
-      Bývalý pivovar Sedlec u katedrály Panny Marie a sv. Jana Křtitele v Kutné Hoře je již několik let regionálním centrem nezávislé kultury. Druhý červencový víkend jej zamýšlíme zahltit maratonem studentského divadla.  Areál nabízí desítku originálních lokací, těšit se můžete nejen na openair, ale i komorní inscenace v interiérech stodol a ve spletitém podzemí. V jedinečném industriálním objektu vzniknou divadelní projekty přímo na míru jednotlivým prostorům, ale můžete se těšit i na reprízy oblíbených inscenací z repertoáru. Dramaturgie festivalu bude velmi různorodá, uvedeme interpretace klasických textů, díla současných dramatiků, ale i autorské inscenace.
-    </p>
-    <p>
-         <a href="https://www.goforboom.com/cs/events/oldstars-on-the-road-kutna-hora?fbclid=IwAR0RX5qLeQUuzG-G--keDNZDRGwVAGxHXxMJ-UFizc4Eff1zKRS-bDtKDuA">Vstupenky na festival k zakoupení zde</a>
-    </p>
-    <p> Projekt OLDstars on the ROAD 2022 byl podpořen grantem Státního fondu kultury, grantem měst Kutná Hora a Roudnice nad Labem a grantem Středočeského kraje - děkujeme!</p> 
-  
-    <p>Letošní ročník proběhne pod záštitou rektora Janáčkovy akademie múzických umění v Brně MgA. Petra Michálka, radního pro oblast kultury, památkové péče a cestovního ruchu Středočeského kraje Mgr. Václava Švendy, radního pro kulturu v Kutné Hoře Mgr. Štěpána Drtiny a vedoucího Katedry pedagogiky na Filozofické fakultě Univerzity Karlovy v Praze Doc. PhDr. Martina Strouhala, Ph.D. Děkujeme! </p>
-    <p>
-         <a href="http://www.oldstarsontheroad.cz">www.oldstarsontheroad.cz</a>
-    </p>
-    <h3>Jak se dělá divadelní festival?</h3>
-    <p> 
-      <i>Doprovodný program OLDstars v Kutné Hoře</i>
-    </p>
-    <p>
-      Program festivalu bude bohatý, inscenovány budou nejen hry klasické, ale i nejnovější kousky od současných dramatiků, nabídnou i pestrou škálu doprovodných programů. Organizovány budou workshopy herectví, cirkusové akrobacie a příměstský tábor "Jak se dělá divadelní festival?".
-    </p>
-    <p>
-      Letní divadelní festival si pochopitelně žádá celou řadu příprav. Účastníci příměstského tábora tak budou mít jedinečnou možnost začlenit se do tvorby festivalu od samého začátku. Každý den přípravného týdne (4.-8. 7. 2022) je pro ně připraven jeden ateliér, který jim umožní nahlédnout na přípravy festivalu optikou nejrůznějších divadelních profesí. Těšit se tak mohou například na seznámení s výkonnou produkcí, propagací a PR, tvůrčím psaním nebo ovládáním divadelní techniky. Dále se zúčastní vybraných divadelních zkoušek a samozřejmě si uměleckou práci vyzkouší na vlastní kůži. Předchozí zkušenost s divadlem není nutná. 
-    </p>
-    <p>
-      Podrobnější informace a přihlášky na e-mailové adrese festival@oldstars.cz. Součástí workshopů i příměstského tábora je festivalová permanentka. Tábor je primárně určen pro účastníky ve věku 13-17 let.
-    </p>
-    <p>
-      Partnery festivalu OLDstars on the ROAD: Kutná Hora 2022 jsou <a href="https://www.divadlox10.cz/cs/kutna-hora">X10 Kutná Hora</a>, turistická oblast <a href="https://www.kutnohorskokolinsko.cz/">Kutnohorskokolínsko</a>, reklamní agentura <a href="https://lepor.cz/">Lepor</a> a oldservis.cz.
-      Mediálními partnery festivalu jsou servery <a href="https://www.mistnikultura.cz/">Místní kultura</a>, <a href="https://www.i-divadlo.cz/">i-divadlo.cz</a>, <a href="http://www.scena.cz/">Scena.cz</a> a <a href="https://www.kudyznudy.cz/">Kudyznudy.cz</a>.
+      Další ročník tradičního putovního benefičního léta se studentským divadlem získává obrysy. Začátek července (7.-9. 7.) chystáme původní divadelní openair projekt do bývalé vodní nádrže Sukov u Kutné Hory. V pondělí 10.
+ a v úterý 11. 7. budeme divadelní přehlídkou okupovat hrad Točník. Prodloužený víkend 13.-16. 7. otestujeme novou nečekanou lokaci v Praze, kde uvedeme několik původních premiér. Následovat do konce července bude turné po hradech, zámcích, klášterech, zahradách a parcích - zejména Středočeského kraje.
     </p>  
   </Layout>
 )
