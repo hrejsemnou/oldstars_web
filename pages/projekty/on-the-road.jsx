17bb32248c9d6509eb2afe0fd2eb11a339719e4b
klaravank <93821300+klaravank@users.noreply.github.com>
2023-02-16 11:12:01 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -4,8 +4,10 @@ const Home = () => (
   <Layout title="OLDstars > Projekty > OLDstars on the ROAD">
     <h2>OLDstars on the ROAD 2023</h2>
     <p>
-      Další ročník tradičního putovního benefičního léta se studentským divadlem získává obrysy. Začátek července (7.-9. 7.) chystáme původní divadelní openair projekt do bývalé vodní nádrže Sukov u Kutné Hory. V pondělí 10.
- a v úterý 11. 7. budeme divadelní přehlídkou okupovat hrad Točník. Prodloužený víkend 13.-16. 7. otestujeme novou nečekanou lokaci v Praze, kde uvedeme několik původních premiér. Následovat do konce července bude turné po hradech, zámcích, klášterech, zahradách a parcích - zejména Středočeského kraje.
+      Další ročník tradičního putovního benefičního léta se studentským divadlem získává obrysy. Začátek července (7.-9. 7.) chystáme původní divadelní openair projekt do bývalé vodní nádrže Sukov u Kutné Hory. 
+    </p>
+    <p>
+      V pondělí 10. a v úterý 11. 7. budeme divadelní přehlídkou okupovat hrad Točník. Prodloužený víkend 13.-16. 7. otestujeme novou nečekanou lokaci v Praze, kde uvedeme několik původních premiér. Následovat do konce července bude turné po hradech, zámcích, klášterech, zahradách a parcích - zejména Středočeského kraje.
     </p>  
   </Layout>
 )
