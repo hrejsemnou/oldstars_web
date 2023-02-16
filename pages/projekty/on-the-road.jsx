9e31f26c53368ca9a5fe2ef79cd879868be1807d
klaravank <93821300+klaravank@users.noreply.github.com>
2023-02-16 11:08:17 +0100

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -3,9 +3,6 @@ import Layout from '../../components/Layout'
 const Home = () => (
   <Layout title="OLDstars > Projekty > OLDstars on the ROAD">
     <h2>OLDstars on the ROAD 2023</h2>
-    <h4><a href="https://www.oldstarsontheroad.cz/festival/">8.- 10.7. 2022 bývalý pivovar Sedlec, Kutná Hora</a></h4>
-    <h4><a href="https://www.oldstarsontheroad.cz/turne/">11.-15. 7. turné po hradech, zámcích a okolí</a></h4>
-    <h4><a href="https://www.oldstarsontheroad.cz/roudnice/">16.-17.7. klášter Roudnice nad Labem</a></h4>
     <p>
       Další ročník tradičního putovního benefičního léta se studentským divadlem získává obrysy. Začátek července (7.-9. 7.) chystáme původní divadelní openair projekt do bývalé vodní nádrže Sukov u Kutné Hory. V pondělí 10.
  a v úterý 11. 7. budeme divadelní přehlídkou okupovat hrad Točník. Prodloužený víkend 13.-16. 7. otestujeme novou nečekanou lokaci v Praze, kde uvedeme několik původních premiér. Následovat do konce července bude turné po hradech, zámcích, klášterech, zahradách a parcích - zejména Středočeského kraje.
