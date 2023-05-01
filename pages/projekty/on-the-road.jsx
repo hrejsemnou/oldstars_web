3e7b07447085f1696e53b4e79885508797af5f5d
klaravank <93821300+klaravank@users.noreply.github.com>
2023-05-01 21:32:43 +0200

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -4,14 +4,32 @@ const Home = () => (
   <Layout title="OLDstars > Projekty > OLDstars on the ROAD">
     <h2>OLDstars on the ROAD 2023</h2>
     <p>
-      Další ročník tradičního putovního benefičního léta se studentským divadlem získává obrysy. Začátek července (7.-9. 7.) chystáme původní divadelní openair projekt do bývalé vodní nádrže Sukov u Kutné Hory. 
+      Další ročník putovního benefičního letního studentského projektu.
     </p>
     <p>
-      V pondělí 10. a v úterý 11. 7. budeme divadelní přehlídkou okupovat hrad Točník. Prodloužený víkend 13.-16. 7. otestujeme novou nečekanou lokaci v Praze, kde uvedeme několik původních premiér. Následovat do konce července bude turné po hradech, zámcích, klášterech, zahradách a parcích - zejména Středočeského kraje.
-    </p> 
+      V sobotu 8. července 20:00 v jediném provedení představíme v unikátní lokaci bývalé vodní nádrže Sukov u Kutné Hory původní openair inscenaci textu hvězdy evropského divadla Maria Von Mayenburga MARS. 
+    </p>
     <p>
-        Projekt byl podpořen grantem Města Kutná Hora a grantem Státního fondu kultury - děkujeme! Záštitu nad projektem převzal radní města Kutná Hora Tomáš Morawski a rektor JAMU pan MgA. Petr Michálek. Na projektu spolupracuje dobrovolnické centrum Univerzity Karlovy. Za podporu dále děkujeme firmám: Stagelab, Magnetic, Lepor, Vektra, RC Europe a Technickým službám města Kutná Hora. Autorská práva zastupují Dilia a Aurapont. Oficiální  předprodejcem vstupenek je Goout.
-    </p>    
+      Oblíbené monodrama MARGARITĚ s Ljubou Krbovou uvedeme pohostinsky  v aule bývalého kláštera svaté Voršily (dnes Církevní gymnázium) v Kutné Hoře v neděli 9. 7. 18:00. 
+    </p>
+    <p>
+      Veřejná prostranství historického centra Kutné Hory o víkendu 7. - 9. 7. naplníme sérií repríz inscenace dochovalého torza středověké hry MASTIČKÁŘ.
+    </p>
+    <p>
+      Od nedělního večera 9. 7. do úterního večera 11. 7. budeme marathonem studentského divadla okupovat hrad Točník. 
+    </p>
+    <p>
+      Prodloužený víkend 13. - 16. 7. otestujeme divadlem novou nečekanou lokaci v centru Prahy –podkroví bývalé solné věže, kde uvedeme původní premiéry slavných her: VOJCEK a CALIGULA. 
+    </p>
+    <p>
+      Následovat do konce července bude turné s openair inscenací HAMLET po hradech, zámcích, klášterech, zahradách a parcích - zejména Středočeského kraje.
+    </p>
+    <p>
+      <a href="https://www.oldstarsontheroad.cz/">www.oldstarsontheroad.cz</a>
+    </p>
+    <p>
+      Projekt byl podpořen grantem Města Kutná Hora a grantem Státního fondu kultury - děkujeme! Záštitu nad projektem převzal radní města Kutná Hora Tomáš Morawski a rektor JAMU pan MgA. Petr Michálek. Na projektu spolupracuje dobrovolnické centrum Univerzity Karlovy. Za podporu dále děkujeme firmám: Stagelab, Magnetic, Lepor, Vektra a RC Europe. Autorská práva zastupují Dilia a Aurapont. Oficiální předprodejcem vstupenek je Goout.
+    </p>  
   </Layout>
 )
 
