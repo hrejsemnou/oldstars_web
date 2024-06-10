c3adef02131b571ba4d20ad52032d423e2fb19a6
Tomáš Staněk <98696848+tomasstan@users.noreply.github.com>
2024-06-10 16:04:20 +0200

    Update festival-studentskych-divadel.jsx

--- pages/projekty/festival-studentskych-divadel.jsx
+++ pages/projekty/festival-studentskych-divadel.jsx
@@ -9,14 +9,14 @@ const Home = () => (
     <p>
       Festival je nízkorozpočtový a tradičně je podporován grantem Městské části Prahy 1 a Státním fondem kultury - děkujeme. 
     </p>
-    <h3>Letošní ročník 2024 proběhne v termínu 26.-28. října 2024 </h3>
+    <h3>Letošní ročník 2024 proběhne v termínu 26.-28. října 2024. Program bude zveřejněn posledního srpna a v předprodeji na webu Divadla v Celetné a Goout. </h3>
     <p>
      
     </p>
     <h3>Kontakty</h3>
     <p>
     <div>
-      produkce festivalu
+      koordinátor festivalu
     </div>
     <div>
       Jiří Dejl, +420 608 165 789, <a href="mailto:jiri.dejl@oldstars.cz">jiri.dejl@oldstars.cz</a>
