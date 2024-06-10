47625ed5124f113c92c36afe9759d5200b2a4880
Tomáš Staněk <98696848+tomasstan@users.noreply.github.com>
2024-06-10 14:39:52 +0200

    Update festival-studentskych-divadel.jsx

--- pages/projekty/festival-studentskych-divadel.jsx
+++ pages/projekty/festival-studentskych-divadel.jsx
@@ -4,59 +4,14 @@ const Home = () => (
   <Layout title="OLDstars > Projekty > Festival studentských divadel">
     <h2>Program minulého ročníku 2023 k nahlédnutí zde:</h2>
     <p>
-      Podzimní Festival studentských divadel pořádáme již více než 10 let. Třídenní maraton soudobé tvorby na pražské prestižní adrese v Divadle Celetná je vyhledávanou platformou studentského divadla. Prostor dostávají i spřátelené studentské divadelní soubory - v minulosti to byly například Dismanův rozhlasový dětský soubor, DDM Praha 3, Vanabí Litoměřice, Divadelní spolek JAK, spolek Nepřijatelní, Divadelní studio gymnázia Jaroslava Heyrovského, Divadelní ateliér Ulita a mnoho dalších. 
+      Podzimní Festival studentských divadel pořádáme již více než 10 let. Třídenní maraton soudobé tvorby na pražské prestižní adrese v Divadle Celetná a na půdě bývalé Solné věže je vyhledávanou platformou studentského divadla. Prostor dostávají i spřátelené studentské divadelní soubory - v minulosti to byly například Dismanův rozhlasový dětský soubor, DDM Praha 3, Vanabí Litoměřice, Divadelní spolek JAK, spolek Nepřijatelní, Divadelní studio gymnázia Jaroslava Heyrovského, Divadelní ateliér Ulita a mnoho dalších. 
     </p>
     <p>
       Festival je nízkorozpočtový a tradičně je podporován grantem Městské části Prahy 1 a Státním fondem kultury - děkujeme. 
     </p>
-    <h3>Letošní ročník 2022</h3>
+    <h3>Letošní ročník 2024 proběhne v termínu 26.-28. října 2024 </h3>
     <p>
-      Letošní ročník pořádáme tradičně poslední říjnový víkend v Divadle v Celetné. Festivalová přehlídka studentského divadla proběhne ve dnech 27.-29.10.2022.
-    </p>
-    <h4>  ČTVRTEK 27. 10. </h4>
-    <p>
-        18:00 <a href="https://www.oldstars.cz/repertoar/dopisy%20k%20pat%C4%9B">DOPISY K PATĚ STROMU</a>
-    </p>
-    <p>
-        20:00 <a href="https://www.oldstars.cz/repertoar/Habbat%20alep">HABBAT ALEP</a>
-    </p>
-    <p>
-        22:00 <a href="https://www.oldstars.cz/repertoar/balkon">BALKON</a> 
-    </p>
-    <h4> PÁTEK 28. 10. – státní svátek </h4>
-    <p>   
-        15:00 <a href="https://www.oldstars.cz/repertoar/Dokonal%C3%BD%20partner">DOKONALÝ PARTNER  - Vanabí, Litoměřice</a>
-    </p>
-    <p>
-        17:00 <a href="https://www.oldstars.cz/repertoar/vladimir">VLADIMÍR</a>
-    </p>
-    <p>
-        19:00 JEN PRO TVÉ DOBRO - DDM  Prahy 3
-    </p>
-    <p>
-        21:00 <a href="https://www.oldstars.cz/repertoar/Macbeth22">MACBETH</a>
-    </p>
-    <p>
-        23:00 <a href="https://www.oldstars.cz/repertoar/za%20zrcadlem">ZA ZRCADLEM</a>
-    </p>  
-    <h4> SOBOTA 29. 10. </h4>
-    <p>
-        15:00 <a href="https://www.oldstars.cz/repertoar/bose-nohy">BOSÉ NOHY</a>
-    </p>
-    <p>
-        17:00 <a href="https://www.oldstars.cz/repertoar/opil%C3%AD">OPILÍ</a>
-    </p>
-    <p>
-        19:00 <a href="https://www.oldstars.cz/repertoar/plice">PLÍCE</a>
-    </p>
-    <p>
-        21:00 <a href="https://www.oldstars.cz/repertoar/100-songu">100 SONGŮ</a>
-    </p>
-    <p>
-        22:00 <a href="https://www.oldstarsontheroad.cz/mejdan-majdanu/">MEJDAN MAJDANU</a>
-    </p>
-    <p>
-        23:00 <a href="https://www.oldstars.cz/repertoar/vztekli-psi">VZTEKLÍ PSI - derniéra</a>
+     
     </p>
     <h3>Kontakty</h3>
     <p>
@@ -67,16 +22,7 @@ const Home = () => (
       Jiří Dejl, +420 608 165 789, <a href="mailto:jiri.dejl@oldstars.cz">jiri.dejl@oldstars.cz</a>
     </div>
     <br />
-    <div>
-      PR a propagace
-    </div>
-    </p>
-    <h3>Proběhlé ročníky</h3>
-    <p>Omlouváme se, na této sekci se pracuje</p>
-    
-    <h3>Články</h3>
-    <p>Omlouváme se, na této sekci se pracuje</p>
-  </Layout>
+
 )
 
 export default Home
