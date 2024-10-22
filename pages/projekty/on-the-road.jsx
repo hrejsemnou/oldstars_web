1a0c84fe419dd2ab103dfeb253c3de5f28404586
klaravank <93821300+klaravank@users.noreply.github.com>
2024-10-22 22:17:39 +0200

    Update on-the-road.jsx

--- pages/projekty/on-the-road.jsx
+++ pages/projekty/on-the-road.jsx
@@ -7,33 +7,17 @@ const Home = () => (
       Další ročník putovního benefičního letního studentského projektu.
     </p>
     <p>
-      Letošní ročník našeho putovního divadelního léta podělíme mezi Kutnou Horu, hrad Točník, Klášter Roudnice nad Labem a desítku dalších lokací ve Středočeském kraji.
-
-Do svých diářů můžete rezervovat první termíny:
-
-Sobota 6/7 AN/NA - jednorázové uvedení původního divadelního projektu v Klášteře v Roudnici nad Labem
-
-Pátek 12/7 20:00 ROMEO A JULIE v Kutné Hoře, premiéra openair cestovní divadelní inscenace, start turné pro Středočeském kraji
-
-Čtvrtek 18/7 20:00 ROMEO A JULIE v klášteře v Roudnici nad Labem
-
-Pátek 19/7 20:00 ROMEO A JULIE na hradě Točník
-
-Sobota 27/7 20:00 PÍSKOVIŠTĚ v klášteře v Roudnici nad Labem
-
-Neděle 28/7 18:00 PÍSKOVIŠTĚ v Benešově
-
-Pondělí 29/7 20:00 PÍSKOVIŠTĚ v Kutné Hoře
-
-Čtvrtek 19/9 cca 21:00 MARIE STUARTOVNA - premiéra původního divadelního projektu pro park pod Vlašským dvorem v Kutné Hoře v rámci Divadelního festivalu divadla X10.
+      Ročník 2024 našeho putovního divadelního léta jsme podělili mezi Kutnou Horu, hrad Točník, Klášter Roudnice nad Labem a desítku dalších lokací ve Středočeském kraji.
     </p>
     <p>
       <a href="https://www.oldstarsontheroad.cz/">www.oldstarsontheroad.cz</a>
     </p>
     <p>
       Projekt OLDstars on the ROAD 2024 byl podpořen grantem města Kutná Hora a Středočeským krajem - děkujeme.
-
-Mediálními partnery jsou: Kudyznudy.cz, Horažije.cz, KutnohorskoKolínsko.cz, Scéna.cz a Kutnohorské léto.</p>  
+    </p>
+    <p>
+      Mediálními partnery jsou: Kudyznudy.cz, Horažije.cz, KutnohorskoKolínsko.cz, Scéna.cz a Kutnohorské léto.
+    </p>  
   </Layout>
 )
 
