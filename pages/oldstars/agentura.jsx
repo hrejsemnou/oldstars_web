87d4514008d78d485edf03a9cfe3cd02a7fc00ef
klaravank <93821300+klaravank@users.noreply.github.com>
2026-03-11 20:09:10 +0100

    Add files via upload

---
+++ pages/oldstars/agentura.jsx
@@ -0,0 +1,55 @@
+import Layout from '../../components/Layout'
+
+const Home = () => (
+  <Layout title="OLDstars > Agentura">
+    <h2>Agentura</h2>
+    <h3>Castingoví agentura OLDstars</h3>
+    <article>
+      <p>
+        Castingová agentura <strong>OLDstars</strong> propojuje mladé talentované herce s profesionálními příležitostmi ve filmu, televizi, divadle i reklamě. Vychází z více než dvacetileté zkušenosti umělecké skupiny OLDstars, která systematicky pracuje s mladými umělci a připravuje je na profesionální kariéru.
+      </p>
+      <p>
+        Zastupujeme vybrané herce a herečky z komunity OLDstars – studenty  i čerstvé absolventy, kteří mají zkušenost s intenzivní hereckou průpravou, jevištní praxí i prací před kamerou. 
+      </p>
+    </article>
+    <h3>Co nabízíme producentům</h3>
+    <article>
+      <p>
+        Spolupracujeme s filmovými, televizními i divadelními produkcemi.
+      </p>
+      <p>
+        <li> obsazování menších i větších hereckých rolí mladými talenty</li>
+        <li> tipování nových neokoukaných tváří pro film, seriály a reklamy</li>
+        <li> rychlé zajištění herců podle požadovaného typu</li>
+        <li> castingové fotografie a videoukázky</li>
+        <li> administrativní zajištění a koordinace spolupráce s umělci prostřednictvím agentury</li>
+      </p>
+      <p>
+        Díky široké komunitě OLDstars jsme schopni nabídnout mladé <strong>herce různých typů</strong>.
+      </p>  
+    </article>
+    <h3>Naši herci</h3>
+    <article>
+      <p>
+        Herci zastupovaní agenturou OLDstars procházejí systematickou přípravou v rámci projektů a inscenací. Pravidelně se účastní workshopů. Mnozí z nich pokračují ve studiu na uměleckých školách (DAMU, JAMU, konzervatoře aj.) nebo již působí v profesionálních divadlech a audiovizuálních projektech.
+      </p> 
+    </article>
+    <h3>Proč spolupracovat s OLDstars</h3>
+    <article>
+      <p>
+        <li> dlouhodobá práce s mladými talenty</li>
+        <li> silné zázemí umělecké skupiny a pedagogického vedení</li>
+        <li> flexibilní a rychlá komunikace</li>
+      </p>
+      <p>
+        Naším cílem je vytvářet prostředí, které pomáhá mladým umělcům vstupovat do profesionálního světa a zároveň nabízí producentům spolehlivý přístup k novým talentům.
+      </p> 
+      <p>
+      Matěj Zahajský - matej.zahajsky@oldstars.cz<br>
+        +420 602 371 223
+      </p>
+    </article>
+  </Layout>
+)
+
+export default Home
